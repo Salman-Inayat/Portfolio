e1104e2a860ce6849a86cbd0396684f978781347
Salman-Inayat <salmaninayat1999@gmail.com>
2021-07-26 11:30:53 +0500

    Skills section added

--- pages/Skills.js
+++ pages/Skills.js
@@ -1,11 +1,59 @@
-import React from 'react'
+import React from "react";
+import styles from "../styles/Skills.module.css";
 
 const Skills = () => {
-    return (
-        <div>
-            <h1>Skills </h1>
+  const skills = [
+    {
+      name: "HTML",
+      percent: "95%",
+    },
+    {
+      name: "CSS",
+      percent: "90%",
+    },
+    {
+      name: "JS",
+      percent: "95%",
+    },
+    {
+      name: "React.js",
+      percent: "80%",
+    },
+    {
+      name: "Node.js",
+      percent: "70%",
+    },
+    {
+      name: "Mongodb",
+      percent: "70%",
+    },
+  ];
+
+  const Skill_bars = () => {
+    return skills.map((skill) => {
+      return (
+        <div className={styles.skills}>
+          <span className={styles.name}>{skill.name}</span>
+          <div className={styles.percent}>
+            <div
+              className={styles.progress}
+              style={{ width: `${skill.percent}` }}
+            ></div>
+          </div>
+          <span className={styles.value}>{skill.percent}</span>
         </div>
-    )
-}
+      );
+    });
+  };
+
+  return (
+    <div className={styles.body}>
+      <div className={styles.container}>
+        <h1>Skills </h1>
+        <Skill_bars />
+      </div>
+    </div>
+  );
+};
 
 export default Skills;
