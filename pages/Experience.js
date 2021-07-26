2f0d842b6786869b6d818a66c53e845e2f95c621
Salman-Inayat <salmaninayat1999@gmail.com>
2021-07-26 11:31:03 +0500

    Experience timeline added

--- pages/Experience.js
+++ pages/Experience.js
@@ -1,52 +1,108 @@
-import React from "react"
-  import { Chrono } from "react-chrono";
-  
-  const Experience = () => {
-    const items = [{
-      title: "May 1940",
-      cardTitle: "Dunkirk",
-      cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
-      cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
-    },
-    {
-        title: "May 1940",
-        cardTitle: "Dunkirk",
-        cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
-        cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
-      },
-      {
-        title: "May 1940",
-        cardTitle: "Dunkirk",
-        cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
-        cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
-      },
-      {
-        title: "May 1940",
-        cardTitle: "Dunkirk",
-        cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
-        cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
-      }
-      ,
-      {
-        title: "May 1940",
-        cardTitle: "Dunkirk",
-        cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
-        cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
-      }
-      ,
-      {
-        title: "May 1940",
-        cardTitle: "Dunkirk",
-        cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
-        cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
-      }];
+import React from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import GitHubIcon from "@material-ui/icons/GitHub";
 
-    return (
-      <div style={{ width: "100%", height: "100%" }}>
-        <Chrono slideshow items={items} mode="VERTICAL_ALTERNATING"/>
-      </div>
-    )
-  }
+const Experience = () => {
+  return (
+    <VerticalTimeline>
+      <VerticalTimelineElement
+        className="vertical-timeline-element--work"
+        contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+        contentArrowStyle={{ borderRight: "15px solid  rgb(33, 150, 243)" }}
+        date="2011 - 2021"
+        iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+        icon={<GitHubIcon />}
+      >
+        <h3 className="vertical-timeline-element-title">Creative Director</h3>
+        <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
+        <p>
+          Creative Direction, User Experience, Visual Design, Project
+          Management, Team Leading
+        </p>
+      </VerticalTimelineElement>
+      <VerticalTimelineElement
+        className="vertical-timeline-element--work"
+        date="2010 - 2011"
+        iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+        icon={<GitHubIcon />}
+      >
+        <h3 className="vertical-timeline-element-title">Art Director</h3>
+        <h4 className="vertical-timeline-element-subtitle">
+          San Francisco, CA
+        </h4>
+        <p>
+          Creative Direction, User Experience, Visual Design, SEO, Online
+          Marketing
+        </p>
+      </VerticalTimelineElement>
+      <VerticalTimelineElement
+        className="vertical-timeline-element--work"
+        date="2008 - 2010"
+        iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+        icon={<GitHubIcon />}
+      >
+        <h3 className="vertical-timeline-element-title">Web Designer</h3>
+        <h4 className="vertical-timeline-element-subtitle">Los Angeles, CA</h4>
+        <p>User Experience, Visual Design</p>
+      </VerticalTimelineElement>
+      <VerticalTimelineElement
+        className="vertical-timeline-element--work"
+        date="2006 - 2008"
+        iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+        icon={<GitHubIcon />}
+      >
+        <h3 className="vertical-timeline-element-title">Web Designer</h3>
+        <h4 className="vertical-timeline-element-subtitle">
+          San Francisco, CA
+        </h4>
+        <p>User Experience, Visual Design</p>
+      </VerticalTimelineElement>
+      <VerticalTimelineElement
+        className="vertical-timeline-element--education"
+        date="April 2013"
+        iconStyle={{ background: "rgb(233, 30, 99)", color: "#fff" }}
+        icon={<GitHubIcon />}
+      >
+        <h3 className="vertical-timeline-element-title">
+          Content Marketing for Web, Mobile and Social Media
+        </h3>
+        <h4 className="vertical-timeline-element-subtitle">Online Course</h4>
+        <p>Strategy, Social Media</p>
+      </VerticalTimelineElement>
+      <VerticalTimelineElement
+        className="vertical-timeline-element--education"
+        date="November 2012"
+        iconStyle={{ background: "rgb(233, 30, 99)", color: "#fff" }}
+        icon={<GitHubIcon />}
+      >
+        <h3 className="vertical-timeline-element-title">
+          Agile Development Scrum Master
+        </h3>
+        <h4 className="vertical-timeline-element-subtitle">Certification</h4>
+        <p>Creative Direction, User Experience, Visual Design</p>
+      </VerticalTimelineElement>
+      <VerticalTimelineElement
+        className="vertical-timeline-element--education"
+        date="2002 - 2006"
+        iconStyle={{ background: "rgb(233, 30, 99)", color: "#fff" }}
+        icon={<GitHubIcon />}
+      >
+        <h3 className="vertical-timeline-element-title">
+          Bachelor of Science in Interactive Digital Media Visual Imaging
+        </h3>
+        <h4 className="vertical-timeline-element-subtitle">Bachelor Degree</h4>
+        <p>Creative Direction, Visual Design</p>
+      </VerticalTimelineElement>
+      <VerticalTimelineElement
+        iconStyle={{ background: "rgb(16, 204, 82)", color: "#fff" }}
+        icon={<GitHubIcon />}
+      />
+    </VerticalTimeline>
+  );
+};
 
-
-  export default Experience
+export default Experience;
