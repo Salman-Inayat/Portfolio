0aa948dbeaf14398a1e283430f061e45151d9ca8
Salman-Inayat <salmaninayat1999@gmail.com>
2021-08-25 10:55:28 +0500

    Header component added

---
+++ components/Header.js
@@ -0,0 +1,169 @@
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  makeStyles,
+  Button,
+  IconButton,
+  Drawer,
+  Link,
+  MenuItem,
+} from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
+import React, { useState, useEffect } from "react";
+import Image from "next/image";
+
+const headersData = [
+  {
+    label: "About Me",
+    href: "AboutMe.js",
+  },
+  {
+    label: "Skills",
+    href: "Skills",
+  },
+  {
+    label: "Projects",
+    href: "Projects",
+  },
+  {
+    label: "Contact",
+    href: "Contact",
+  },
+];
+
+const useStyles = makeStyles(() => ({
+  header: {
+    backgroundColor: "#400CCC",
+    paddingRight: "79px",
+    paddingLeft: "118px",
+    "@media (max-width: 900px)": {
+      paddingLeft: 0,
+    },
+  },
+  logo: {
+    fontFamily: "Work Sans, sans-serif",
+    fontWeight: 600,
+    color: "#FFFEFE",
+    textAlign: "left",
+  },
+  menuButton: {
+    fontFamily: "Open Sans, sans-serif",
+    fontWeight: 700,
+    size: "18px",
+    marginLeft: "38px",
+  },
+  toolbar: {
+    display: "flex",
+    justifyContent: "space-between",
+  },
+  drawerContainer: {
+    padding: "20px 30px",
+  },
+  linkContainer: {
+    display: "flex",
+  },
+}));
+
+export default function Header() {
+  const classes = useStyles();
+
+  const [state, setState] = useState({
+    mobileView: false,
+    drawerOpen: false,
+  });
+
+  const { mobileView, drawerOpen } = state;
+
+  useEffect(() => {
+    const setResponsiveness = () => {
+      return window.innerWidth < 900
+        ? setState((prevState) => ({ ...prevState, mobileView: true }))
+        : setState((prevState) => ({ ...prevState, mobileView: false }));
+    };
+
+    setResponsiveness();
+
+    window.addEventListener("resize", () => setResponsiveness());
+
+    return () => {
+      window.removeEventListener("resize", () => setResponsiveness());
+    };
+  }, []);
+
+  const displayDesktop = () => {
+    return (
+      <Toolbar className={classes.toolbar}>
+        {femmecubatorLogo}
+        <div className={classes.linkContainer}>{getMenuButtons()}</div>
+      </Toolbar>
+    );
+  };
+
+  const displayMobile = () => {
+    const handleDrawerOpen = () =>
+      setState((prevState) => ({ ...prevState, drawerOpen: true }));
+    const handleDrawerClose = () =>
+      setState((prevState) => ({ ...prevState, drawerOpen: false }));
+
+    return (
+      <Toolbar>
+        <IconButton
+          {...{
+            edge: "start",
+            color: "inherit",
+            "aria-label": "menu",
+            "aria-haspopup": "true",
+            onClick: handleDrawerOpen,
+          }}
+        >
+          <MenuIcon />
+        </IconButton>
+
+        <Drawer
+          {...{
+            anchor: "left",
+            open: drawerOpen,
+            onClose: handleDrawerClose,
+          }}
+        >
+          <div className={classes.drawerContainer}>{getDrawerChoices()}</div>
+        </Drawer>
+
+        <div>{femmecubatorLogo}</div>
+      </Toolbar>
+    );
+  };
+
+  const getDrawerChoices = () => {
+    return headersData.map((data, i) => {
+      return (
+        <Link href={data.label} key={i}>
+          <MenuItem>{data.label}</MenuItem>
+        </Link>
+      );
+    });
+  };
+
+  const femmecubatorLogo = (
+    <Image src={"/salman_inayat.png"} alt="" width={200} height={70} />
+  );
+
+  const getMenuButtons = () => {
+    return headersData.map((data, i) => {
+      return (
+        <Link href={data.label} key={i}>
+          <MenuItem>{data.label}</MenuItem>
+        </Link>
+      );
+    });
+  };
+
+  return (
+    <div>
+      <AppBar className={classes.header}>
+        {mobileView ? displayMobile() : displayDesktop()}
+      </AppBar>
+    </div>
+  );
+}
