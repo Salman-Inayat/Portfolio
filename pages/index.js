73092590d4bbd7bbce4ca3bde9ecf5c883494853
Salman Inayat <45660519+Salman-Inayat@users.noreply.github.com>
2021-09-09 16:45:28 +0500

    Updated Landing Page

--- pages/index.js
+++ pages/index.js
@@ -126,7 +126,7 @@ function Home() {
       >
         <Fade bottom>
           <div className={classes.content_container}>
-            <h3 className={classes.text1}>HI THERE<span><Image src="https://raw.githubusercontent.com/MartinHeinz/MartinHeinz/master/wave.gif" width={40} height={40} alt="Picture of the author"/></span> I'M, </h3>
+            <h3 className={classes.text1}>HI THERE<span><img src="https://raw.githubusercontent.com/MartinHeinz/MartinHeinz/master/wave.gif" width="40" height="40" alt="Picture of the author"/></span> I'M, </h3>
             <h1 className={classes.text2}>SALMAN INAYAT</h1>
             <h2 className={classes.text3}>
               A{" "}
